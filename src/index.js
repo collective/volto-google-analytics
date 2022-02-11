4c9f094ccd7f88500af3e91b50262b31091ca144
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-02-11 16:10:31 +0100

    chore: added default cookie expires to 6 month accordingly to gdpr

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
 
 const trackingCode = process.env.RAZZLE_GA_CODE;
+const cookieExpires =
+  config.settings['volto-google-analytics'].cookieExpires ??
+  6 * 30 * 24 * 60 * 60; // in seconds. Default: 6 month
 
 if (__CLIENT__ && trackingCode) {
   ReactGA.initialize(trackingCode, {
     debug: __DEVELOPMENT__ ?? false,
     gaOptions: {
       anonymizeIp: true,
+      cookieExpires: cookieExpires,
     },
   });
 }
@@ -23,4 +27,4 @@ const useGoogleAnalytics = () => {
 
 export { useGoogleAnalytics };
 
-export default config => config;
+export default (config) => config;

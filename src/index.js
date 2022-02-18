aff2ef0e179efdaa10a336e5c31e965fa301e28a
Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>
2022-02-18 11:23:25 +0100

    fix: fix get trrackingCode

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import config from '@plone/volto/registry';
 
-const trackingCode = window?.env?.RAZZLE_GA_CODE || process.env.RAZZLE_GA_CODE;
 const cookieExpires =
   config.settings?.['volto-google-analytics']?.cookieExpires ??
   6 * 30 * 24 * 60 * 60; // in seconds. Default: 6 month
 
-if (__CLIENT__ && trackingCode) {
-  ReactGA.initialize(trackingCode, {
-    debug: __DEVELOPMENT__ ?? false,
-    gaOptions: {
-      anonymizeIp: true,
-      cookieExpires: cookieExpires,
-    },
-  });
+if (__CLIENT__) {
+  const trackingCode =  window?.env?.RAZZLE_GA_CODE || process.env.RAZZLE_GA_CODE;
+  if(trackingCode){
+    ReactGA.initialize(trackingCode, {
+      debug: __DEVELOPMENT__ ?? false,
+      gaOptions: {
+        anonymizeIp: true,
+        cookieExpires: cookieExpires,
+      },
+    });
+  }
 }
 
 const useGoogleAnalytics = () => {

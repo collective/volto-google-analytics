e46e6d1da6570969496fd5d813999bae4e4a4175
wesleybl <wesleybl@gmail.com>
2023-11-22 15:17:19 -0300

    Add parameter to the hook useGoogleAnalytics to tell whether or not we can create cookies
    On portals that need to follow GDPL,
    cookies can only be created after the user's consent. Then add the
    parameter to control whether or not we create the cookie.

--- src/index.js
+++ src/index.js
@@ -8,39 +8,53 @@ const cookieExpires =
   config.settings?.['volto-google-analytics']?.cookieExpires ??
   6 * 30 * 24 * 60 * 60; // in seconds. Default: 6 month
 
-if (__CLIENT__) {
-  const UA_trackingCode =
-    window?.env?.RAZZLE_GA_CODE || process.env.RAZZLE_GA_CODE;
-  if (UA_trackingCode) {
-    ReactGA.initialize(UA_trackingCode, {
-      debug: __DEVELOPMENT__ ?? false,
-      gaOptions: {
-        anonymizeIp: true,
-        cookieExpires: cookieExpires,
-      },
-    });
-  }
-
-  const GA4_trackingCode =
-    window?.env?.RAZZLE_GA4_CODE || process.env.RAZZLE_GA4_CODE;
-  if (GA4_trackingCode) {
-    ReactGA4.initialize([
-      {
-        testMode: __DEVELOPMENT__ ?? false,
-        trackingId: GA4_trackingCode,
+const initializeGA = () => {
+  if (__CLIENT__) {
+    const UA_trackingCode =
+      window?.env?.RAZZLE_GA_CODE || process.env.RAZZLE_GA_CODE;
+    if (UA_trackingCode) {
+      ReactGA.initialize(UA_trackingCode, {
+        debug: __DEVELOPMENT__ ?? false,
         gaOptions: {
           anonymizeIp: true,
           cookieExpires: cookieExpires,
         },
-      },
-    ]);
-  }
-}
+      });
+    }
 
-const useGoogleAnalytics = () => {
+    const GA4_trackingCode =
+      window?.env?.RAZZLE_GA4_CODE || process.env.RAZZLE_GA4_CODE;
+    if (GA4_trackingCode) {
+      ReactGA4.initialize([
+        {
+          testMode: __DEVELOPMENT__ ?? false,
+          trackingId: GA4_trackingCode,
+          gaOptions: {
+            anonymizeIp: true,
+            cookieExpires: cookieExpires,
+          },
+        },
+      ]);
+    }
+  }
+};
+
+const isGAInitialized = () => {
+  if (__CLIENT__) {
+    return ReactGA4.isInitialized;
+  }
+};
+
+const useGoogleAnalytics = (cookieAllowed = true) => {
   let location = useLocation();
 
   useEffect(() => {
+    if (!cookieAllowed) {
+      return;
+    }
+    if (!isGAInitialized()) {
+      initializeGA();
+    }
     const UA_trackingCode =
       window?.env?.RAZZLE_GA_CODE || process.env.RAZZLE_GA_CODE;
     const GA4_trackingCode =
@@ -52,9 +66,11 @@ const useGoogleAnalytics = () => {
     if (GA4_trackingCode) {
       ReactGA4.send({ hitType: 'pageview', page: location.pathname });
     }
-  }, [location]);
+  }, [location, cookieAllowed]);
 };
+
+const exportDefault = (config) => config;
 
 export { useGoogleAnalytics };
 
-export default (config) => config;
+export default exportDefault;
